3b9c7c88809be3e007b0cd3097ab856bd151908f
Renuka Shinde <renushinde1998@gmail.com>
2023-10-17 23:08:12 +0530

    html & css part done today

---
+++ script.js
@@ -0,0 +1,15 @@
+const addBtn = document.querySelector(".add-btn");
+const modalCont = document.querySelector(".modal-cont");
+
+let addTaskFlag = false;
+addBtn.addEventListener("click", ()=>{
+    //toggle the modal container
+    addTaskFlag = ! addTaskFlag ;
+
+    if(addTaskFlag === true)
+    {
+      modalCont.style.display = "flex";
+    }else{
+      modalCont.style.display = "none";
+    }
+})
